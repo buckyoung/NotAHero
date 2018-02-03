b8282394784d703621f3262bcf03f660cd1dc51e
Buck Young <buckyoung@Professirs-Pro.local>
2018-02-03 15:27:48 -0500

    fist commit

---
+++ Assets/Scripts/Arm.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Arm : MonoBehaviour {
+	public enum ArmToInt {left = 0, right = 1}
+	public ArmToInt respondsTo;
+
+	public Vector3 forceVector = new Vector3(1, 0, 0);
+	private int forceScaling = 600;
+
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		if (Input.anyKeyDown) {
+			if (Input.inputString.GetHashCode() % 2 == (int)respondsTo) {
+				var magnitude = Input.inputString.Length * forceScaling;
+				rb.AddRelativeForce(forceVector * magnitude);
+			}
+		}
+	}
+}
+
