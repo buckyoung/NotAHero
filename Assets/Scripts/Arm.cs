c449508e5d2323119ca3e66351a8e892e3d120c2
Buck Young <buckyoung@Professirs-Pro.local>
2018-02-03 18:56:31 -0500

    Implement hero, animations, keyboard events, and bad guy idling

--- Assets/Scripts/Arm.cs
+++ Assets/Scripts/Arm.cs
@@ -5,23 +5,53 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 
 public class Arm : MonoBehaviour {
-	public enum ArmToInt {left = 0, right = 1}
-	public ArmToInt respondsTo;
+	public enum ArmEnum {left = 0, right = 1}
+	public ArmEnum respondsTo;
 
 	public Vector3 forceVector = new Vector3(1, 0, 0);
-	private int forceScaling = 600;
 
+	private int forceScaling = 600;
 	private Rigidbody rb;
+
+	private Hashtable handHash = new Hashtable();
+	private int[] leftHandChars = new int[] {'1','2','3','4','5','6','`','a','b','c','d','e','f','g','q','r','s','t','v','w','x','z', ' '};
+	private int[] rightHandChars = new int[] {',','-','.','/','0','7','8','9',';','=','[',']','h','i','j','k','l','m','n','o','p','u','y'};
+	private Animator heroAnimator;
+	private Hashtable actionMap = new Hashtable();
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+
+		foreach (var character in leftHandChars) {
+			handHash.Add (character, ArmEnum.left);	
+		}
+
+		foreach (var character in rightHandChars) {
+			handHash.Add (character, ArmEnum.right);	
+		}
+
+		heroAnimator = GameObject.Find ("Hero").GetComponent<Animator> ();
+
+		// Coordinate player actions to handedness
+		actionMap.Add(ArmEnum.left, "Attack");
+		actionMap.Add(ArmEnum.right, "Attack");
 	}
 
 	void Update () {
 		if (Input.anyKeyDown) {
-			if (Input.inputString.GetHashCode() % 2 == (int)respondsTo) {
+			string characters = Input.inputString.ToLower();
+
+			if (characters.Length < 1) {
+				return;
+			}
+
+			int character = characters[0].GetHashCode(); // use the handedness depending on the first character pressed
+
+			if (handHash.ContainsKey(character) && (ArmEnum)handHash[character] == respondsTo) {
 				var magnitude = Input.inputString.Length * forceScaling;
-				rb.AddRelativeForce(forceVector * magnitude);
+				rb.AddRelativeForce(forceVector * magnitude); // add force that scales with how many keys were hit
+
+				heroAnimator.SetTrigger ((string)actionMap [respondsTo]);
 			}
 		}
 	}
---
+++ Assets/Scripts/Screen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+
+public class Screen : MonoBehaviour {
+	public float speed;
+
+	private Renderer r;
+
+	void Start () {
+		r = gameObject.GetComponent<Renderer>();
+	}
+
+	void Update () {
+		Vector2 offset = new Vector2(Time.deltaTime*speed, 0);
+		r.material.mainTextureOffset += offset;
+	}
+}
